5f82a2960d940c7c944ec4292e6edee4ed095573
Samet Zengin <sametzengin@ogr.bandirma.edu.tr>
2024-04-05 23:34:58 +0300

    add soundeffects
    created a new scriptable object named AudioClipRefSO
    created new events for audios in soundmanager class

--- Assets/Scripts/Managers/DeliveryManager.cs
+++ Assets/Scripts/Managers/DeliveryManager.cs
@@ -7,6 +7,8 @@ public class DeliveryManager : MonoBehaviour
 {
     public event EventHandler OnRecipeSpawned;
     public event EventHandler OnRecipeCompleted;
+    public event EventHandler OnRecipeSuccess;
+    public event EventHandler OnRecipeFailed;
 
 
     public static DeliveryManager Instance { get; private set; }
@@ -81,6 +83,7 @@ public class DeliveryManager : MonoBehaviour
                     waitingrecipeSOList.RemoveAt(i);
 
                     OnRecipeCompleted?.Invoke(this, EventArgs.Empty);
+                    OnRecipeSuccess?.Invoke(this, EventArgs.Empty);
 
                     return;
                 }
@@ -88,7 +91,7 @@ public class DeliveryManager : MonoBehaviour
                 
 
         }
-
+        OnRecipeFailed?.Invoke(this, EventArgs.Empty);
         //No matches found
         //player did not deliver a correct recipe
         Debug.Log("player did not deliver a correct recipe");
--- Assets/Scripts/PlayerScripts/Player.cs
+++ Assets/Scripts/PlayerScripts/Player.cs
@@ -8,6 +8,8 @@ public class Player : MonoBehaviour,IKitchenObjectParent
 
     public static Player Instance { get; private set; }
 
+    public event EventHandler OnPickedSomething;
+
     public event EventHandler<OnSelectedCounterChangedEventArgs> OnSelectedCounterChanged;
     public class OnSelectedCounterChangedEventArgs : EventArgs
     {
@@ -169,6 +171,11 @@ public class Player : MonoBehaviour,IKitchenObjectParent
     public void SetKitchenObject(KitchenObject kitchenObject)
     {
         this.kitchenObject = kitchenObject;
+
+        if(kitchenObject != null)
+        {
+            OnPickedSomething?.Invoke(this, EventArgs.Empty);
+        }
     }
 
     public KitchenObject GetKitchenObject()
